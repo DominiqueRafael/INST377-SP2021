f13baf371d4063c9e82b33f6175ff0185fe33bd9
Dominique Rafael <drafael@terpmail.umd.edu>
2021-05-04 18:23:12 -0400

    map

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,5 +1,13 @@
 function mapInit() {
-  let map = L.map('mapid').setView([51.505, -0.09], 13);
+  let map = L.map('mapid').setView([0, 0], 1);
+  L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=HoFRVfZhUhAgENkBcaCO', {
+    tileSize: 512,
+    zoomOffset: -1,
+    minZoom: 1,
+    attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
+    crossOrigin: true
+  }).addTo(map);
+  let marker = L.marker([51.5, -0.09]).addTo(map);
   return map;
 }
 
@@ -20,7 +28,7 @@ async function dataHandler(mapObjectFromFunction) {
   }
   function displayMatch() {
     const matchArray = findMatches(event.target.value, restaurants);
-    
+
     const html = matchArray
       .map(place => {
         console.log(place);
@@ -29,7 +37,7 @@ async function dataHandler(mapObjectFromFunction) {
               <div class="card">
                 <div class="card-content">
                   <div class="content">
-                    <p class="title">${place.name}</p> 
+                    <p class="title">${place.name}</p>
                     <p class="subtitle">${place.category}</p>
                     <p class="address">${place.address_line_1}, ${place.city}, ${place.zip}</p>
                   </div>
@@ -45,9 +53,6 @@ async function dataHandler(mapObjectFromFunction) {
   });
 
   search.addEventListener("change", displayMatch);
-
-  // use your assignment 1 data handling code here
-  // and target mapObjectFromFunction to attach markers
 }
 
 async function windowActions() {
